987591c4b6739ad797a206f6bc980ec6dc78de59
Ricardo de Carvalho <ricardo.carvalho@yes.technology>
2025-04-14 22:25:03 -0300

    feat: Files unused

--- src/constants.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseErrors = {
-  "auth/operation-not-allowed":
-    "The given sign-in provider is disabled for this Firebase project. Enable it in the Firebase console, under the sign-in method tab of the Auth section.",
-  "auth/email-already-in-use":
-    "The email address is already in use by another account.",
-  "auth/popup-closed-by-user":
-    "The popup has been closed by the user before finalizing the operation.",
-};
