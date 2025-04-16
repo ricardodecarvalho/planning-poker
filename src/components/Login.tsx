e9c1909de4b6e3d068c1c433eee4f13e9da20173
Ricardo de Carvalho <ricardo.carvalho@yes.technology>
2025-04-15 22:49:05 -0300

    feat: Add Connection Alert

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import LogoPPK from "../assets/images/logo-planning-poker.svg?react";
 import LogoPPKWhite from "../assets/images/logo-planning-poker-white.svg?react";
 import useThemeContext from "../context/useThemeContext";
+import ConnectionAlert from "./ConnectionAlert";
 
 const LoginWithGoogleButton = styled.button`
   width: 100%;
@@ -19,30 +20,33 @@ const Login = () => {
   const { theme } = useThemeContext();
 
   return (
-    <div className="container">
-      {theme === "light" ? (
-        <LogoPPK className="mx-auto d-block mt-5" width={250} height={250} />
-      ) : (
-        <LogoPPKWhite
-          className="mx-auto d-block mt-5"
-          width={250}
-          height={250}
-        />
-      )}
-      <div className="mt-4 d-flex flex-column align-items-center justify-content-center gap-3">
-        <h1 className="text-center">
-          <span className="text-body-secondary">Welcome to</span>{" "}
-          <span style={{ whiteSpace: "nowrap" }}>{appName}</span>
-        </h1>
-        <LoginWithGoogleButton
-          className={`btn btn-${theme === "dark" ? "light" : "dark"} btn-lg`}
-          onClick={loginWithGoogle}
-          disabled={loadingLoginWithGoogle}
-        >
-          Login with Google to continue
-        </LoginWithGoogleButton>
+    <>
+      <ConnectionAlert />
+      <div className="container">
+        {theme === "light" ? (
+          <LogoPPK className="mx-auto d-block mt-5" width={250} height={250} />
+        ) : (
+          <LogoPPKWhite
+            className="mx-auto d-block mt-5"
+            width={250}
+            height={250}
+          />
+        )}
+        <div className="mt-4 d-flex flex-column align-items-center justify-content-center gap-3">
+          <h1 className="text-center">
+            <span className="text-body-secondary">Welcome to</span>{" "}
+            <span style={{ whiteSpace: "nowrap" }}>{appName}</span>
+          </h1>
+          <LoginWithGoogleButton
+            className={`btn btn-${theme === "dark" ? "light" : "dark"} btn-lg`}
+            onClick={loginWithGoogle}
+            disabled={loadingLoginWithGoogle}
+          >
+            Login with Google to continue
+          </LoginWithGoogleButton>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
